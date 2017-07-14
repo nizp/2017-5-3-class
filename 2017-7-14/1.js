7518daedc579fe93e44f0ee58ca4126e6609fafd
nizp <nzp890524@163.com>
2017-07-14 10:45:35 +0800

    move

--- 2017-7-14/1.js
+++ 2017-7-14/1.js
@@ -1,2 +1,6 @@
 var div = document.getElementById('div');
-
+document.addEventListener('mousemove',move);
+function move(ev){
+	div.style.left = ev.clientX - disX + 'px';
+	div.style.top = ev.clientY - disY + 'px';
+}
