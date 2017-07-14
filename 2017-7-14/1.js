358a7e42d08e36c36f6c8a2101969272859a7864
nizp <nzp890524@163.com>
2017-07-14 10:49:53 +0800

    down2

--- 2017-7-14/1.js
+++ 2017-7-14/1.js
@@ -3,8 +3,6 @@ var div = document.getElementById('div');
 div.addEventListener('mousedown',function(ev){
 	var disX = ev.clientX - this.offsetLeft;
 	var disY = ev.clientY - this.offsetTop;
-	
-	
 });
 
 
